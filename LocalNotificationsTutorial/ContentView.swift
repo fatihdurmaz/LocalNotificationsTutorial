a90b3026d5a459f2c8788380710083347708db17
Fatih DURMAZ <fatih.durmaz@outlook.com>
2024-05-16 21:59:28 +0300

    Initial Commit

---
+++ LocalNotificationsTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalNotificationsTutorial
+//
+//  Created by Fatih Durmaz on 16.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalNotificationsTutorial/LocalNotificationsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsTutorialApp.swift
+//  LocalNotificationsTutorial
+//
+//  Created by Fatih Durmaz on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
